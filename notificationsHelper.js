f1ae92d99b17065ac3186e393569e79f2f36d42e
domwoe <dwoerner@ethz.ch>
2015-05-07 08:25:58 +0200

    notification expires in 1h

--- notificationsHelper.js
+++ notificationsHelper.js
@@ -3,8 +3,8 @@
 var notifications = require('./lib/notifications.js');
 
 
-var device = 'ios';
-var token = '0a956eeaf63df6dbebbef8fdcfe26f76c8e878ec0155432dd02936ac1d6a0f1f';
+var device = 'android';
+var token = 'APA91bGuzUprnzzY-PxXlXnbmZmJy_dUz6j4yNMM_SMzUMyUFanhdsOGGuLvt_E_3-OtWrW6Ir01iRmjVBTkcdlklFhqopyIPiNMTpay64PcSjTzBeg04pUWKrQk6Pyi29RYXdaeqP1h';
 
 var msg = {
 	alert: 'Noch ein Pups',
